adf8ce8d429451161c3eb5a961ab1a36374239e0
kiran <kirangummadi1985@gmail.com>
2022-06-02 03:44:46 +0530

    terraform-roboshop

--- roboshop/main.tf
+++ roboshop/main.tf
@@ -1,10 +1,14 @@
-module  "common" {
-  count         = length(var.COMPONENTS)
+module "common" {
+  //count         = length(var.COMPONENTS)
+  for_each      = var.COMPONENTS
   source        = "./common"
   INSTANCE_TYPE = "t3.micro"
   AMI           = "ami-0ec3252759130198b"
-  COMPONENT     = var.COMPONENTS[count.index]
-  PORT          = var.PORTS[count.index]
+  COMPONENT     = each.value["name"]
+  PORT          = each.value["port"]
 }
 
 
+output "leng" {
+  value = length(var.COMPONENTS)
+}
--- roboshop/vars.tf
+++ roboshop/vars.tf
@@ -1,7 +1,25 @@
+//variable "COMPONENTS" {
+//  default = ["frontend", "catalogue", "mongodb"]
+//}
+//
+//variable "PORTS" {
+//  default = ["80", "8080", "27017"]
+//}
+
 variable "COMPONENTS" {
-  default = ["frontend", "catalogue", "mongodb"]
+  default = {
+    "frontend" = {
+      name = "frontend"
+      port = 80
+    }
+    "mongodb" = {
+      name = "mongodb"
+      port = 27017
+    }
+    "catalogue" = {
+      name = "catalogue"
+      port = 8080
+    }
+  }
 }
 
-variable "PORTS" {
-  default = ["80", "8080", "27017"]
-}
