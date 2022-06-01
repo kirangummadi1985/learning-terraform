cb3871ce3ef74b242a3323abd56d5e4386679d4f
kiran <kirangummadi1985@gmail.com>
2022-06-02 00:17:26 +0530

    learning-terraform

---
+++ 03-resources/main.tf
@@ -0,0 +1,20 @@
+resource "aws_instance" "web" {
+  instance_type = "t3.micro"
+  ami           = "ami-0ec3252759130198b"
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "= 4.14.0"
+    }
+  }
+}
+
+
+
