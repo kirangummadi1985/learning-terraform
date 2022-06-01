1b835b2ba038364ac291e7c19067b30a092bd9c4
kiran <kirangummadi1985@gmail.com>
2022-06-01 23:24:10 +0530

    learning-terraform

--- 02-vars/vars.tf
+++ 02-vars/vars.tf
@@ -1,7 +1,58 @@
 variable "sample" {
-  default = "Hello World"
+  default = "Hello World NEW"
 }
 
 output "sample" {
   value = var.sample
+}
+
+# String Data type
+variable "sample1" {
+  default = "Hello World"
+}
+
+# Number data type
+variable "sample2" {
+  default = 100
+}
+
+# Boolean Data type
+variable "sample3" {
+  default = true
+}
+
+
+variable "sample4" {
+  default = [
+    "Hello",
+    1000,
+    true,
+    "World"
+  ]
+}
+
+variable "sample5" {
+  default = {
+    string  = "Hello",
+    number  = 100,
+    boolean = true
+  }
+}
+
+output "value-in-list" {
+  value = var.sample4[3]
+}
+
+output "value-in-map" {
+  value = var.sample5["number"]
+}
+
+variable "URL" {}
+output "URL" {
+  value = var.URL
+}
+
+variable "SHELL" {}
+output "SHELL" {
+  value = var.SHELL
 }
