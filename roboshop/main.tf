af9f4e329b408b39defc4e6e12ab735f7a8084f0
kiran <kirangummadi1985@gmail.com>
2022-06-02 03:17:45 +0530

    terraform-roboshop

--- roboshop/main.tf
+++ roboshop/main.tf
@@ -1,9 +1,10 @@
-module  "frontend" {
+module  "common" {
+  COUNT         = length(var.COMPONENTS)
   source        = "./common"
   INSTANCE_TYPE = "t3.micro"
   AMI           = "ami-0ec3252759130198b"
-  COMPONENT     = "frontend"
-  PORT          = 80
+  COMPONENT     = var.COMPONENTS[count.index]
+  PORT          = var.PORTS[count.index]
 }
 
 
---
+++ roboshop/vars.tf
@@ -0,0 +1,7 @@
+variable "COMPONENTS" {
+  default = ["frontend", "catalogue", "mongodb"]
+}
+
+variable "PORTS" {
+  default = ["80", "8080", "27017"]
+}
