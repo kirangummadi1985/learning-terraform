c1e2b25ee81c691d38a5a5c8b622db924e4acec4
kiran <kirangummadi1985@gmail.com>
2022-06-02 00:53:24 +0530

    learning-terraform

---
+++ example1/ec2.tf
@@ -0,0 +1,17 @@
+resource "aws_instance" "sample" {
+  instance_type = "t3.micro"
+  ami           = "ami-0ec3252759130198b"
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "= 4.14.0"
+    }
+  }
+}
