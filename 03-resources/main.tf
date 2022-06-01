8a7e9311024bfebcd7d704524cca179613dc1854
kiran <kirangummadi1985@gmail.com>
2022-06-02 00:24:34 +0530

    learning-terraform

--- 03-resources/main.tf
+++ 03-resources/main.tf
@@ -16,5 +16,15 @@ terraform {
   }
 }
 
+output "instance_id" {
+  description = "ID of the EC2 instance"
+  value       = aws_instance.sample.id
+}
+
+output "instance_public_ip" {
+  description = "Public IP address of the EC2 instance"
+  value       = aws_instance.sample.public_ip
+}
+
 
 
